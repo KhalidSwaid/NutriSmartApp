247f0bb11ca01d3f485fb088ecbe568653112946
Khalid Swaid <khalidsw02@gmail.com>
2024-09-19 14:55:45 +0300

    Create SaveRecipesAndDessertsToDatabase.ts to save recipes and desserts for database, atm it's just for printing, to see if the files are connected correctly

---
+++ nutrismart/src/Backend/SaveRecipesAndDessertsToDatabase.ts
@@ -0,0 +1,8 @@
+// SaveRecipesToDatabase.ts
+
+// Function to print a recipe with a given reason and content
+export function SaveRecipesToDatabase(reason: string, content: string): void {
+  console.log("PRINTG THE RECIPE FROM SAVERECIPESANDDESSERTSTODATABASE");
+  console.log(`Reason: ${reason}`);
+  console.log(`Recipe Content:\n${content}`);
+}
